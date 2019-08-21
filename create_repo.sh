629ee068db2aad9e6704e21e38dfcbbf910bca13
dahak_d <dahak.deborah@gmail.com>
2019-08-21 11:21:38 +0200

    Script créant un repo sur Git et GitHub

---
+++ create_repo.sh
@@ -0,0 +1,20 @@
+# @Author: Dahak Déborah <dahak_d>
+# @Date:   21-08-2019
+# @Email:  deborah.dahak@gmail.com
+# @Project: Reprise
+# @Filename: create_repo.sh
+# @Last modified by:   suika
+# @Last modified time: 21-08-2019
+
+
+
+#!/bin/sh
+# Ce script crée un repo Git et le lie à un repo GitHub
+
+git init;
+echo "Veuillez entrer le nom du repo GitHub : ";
+read repo;
+echo "Veuillez entrer votre mot de passe GitHub : ";
+curl -u 'AoyagiSuika' https://api.github.com/user/repos -d' {"name":"$repo"} ';
+git remote add origin git@github.com:AoyagiSuika/$repo.git;
+echo "Le repo $repo a bien été créé.";
